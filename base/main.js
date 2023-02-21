66fde177121c97e9fe64f654ed80a9f30fcdcc03
lx812833 <lx812833@163.com>
2023-02-21 22:33:05 +0800

    feat：主进程与渲染进程

--- base/main.js
+++ base/main.js
@@ -1,7 +1,7 @@
 const { BrowserWindow, app } = require("electron");
 const path = require("path");
 
-// 创建窗口
+// 创建窗口（主进程---Electron运行环境）
 app.whenReady().then(() => {
   const mainWindow = new BrowserWindow({
     x: 1500,
@@ -14,6 +14,8 @@ app.whenReady().then(() => {
   // 嵌入网页
   // mainWindow.loadURL("https://www.bilibili.com/?spm_id_from=333.1007.0.0")
 
-  // 嵌入本地页面
+  // 嵌入本地页面（渲染进程---浏览器运行环境）
   mainWindow.loadFile(path.resolve(__dirname, "index.html"));
+  // 打开开发者工具
+  mainWindow.webContents.toggleDevTools();
 })
