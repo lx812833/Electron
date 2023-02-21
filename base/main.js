bb4af619a28597c6e1c718887c500b04dfd534e0
lx812833 <lx812833@163.com>
2023-02-21 23:11:06 +0800

    feat：macOS系统激活时事件

--- base/main.js
+++ base/main.js
@@ -2,7 +2,7 @@ const { BrowserWindow, app } = require("electron");
 const path = require("path");
 
 // 创建窗口（主进程---Electron运行环境）
-app.whenReady().then(() => {
+const createWindow = () => {
   const mainWindow = new BrowserWindow({
     x: 1500,
     y: 1000,
@@ -17,5 +17,21 @@ app.whenReady().then(() => {
   // 嵌入本地页面（渲染进程---浏览器运行环境）
   mainWindow.loadFile(path.resolve(__dirname, "index.html"));
   // 打开开发者工具
-  mainWindow.webContents.toggleDevTools();
+  // mainWindow.webContents.toggleDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  // 当所有的窗口都被关闭时触发
+  app.on("window-all-closed", () => {
+    if(process.platform != "darwin") {
+      app.quit();
+    }
+  })
+
+  // macOS 激活时事件
+  app.on("activate", () => {
+    createWindow();
+  })
 })
