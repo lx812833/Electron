d7986d695165744c778de5ed044697207ce2e9ff
longfuyang <longfuyang@aosign.cn>
2023-02-22 17:20:46 +0800

    feat：添加注释

--- base/main.js
+++ base/main.js
@@ -1,14 +1,22 @@
 const { BrowserWindow, app } = require("electron");
 const path = require("path");
 
+/**
+ * app模块：控制应用程序的事件生命周期
+ * BrowserWindow模块：创建和管理应用程序窗口。
+*/
+
 // 创建窗口（主进程---Electron运行环境）
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     x: 1500,
-    y: 1000,
+    y: 100,
     width: 300,
     height: 300,
-    alwaysOnTop: true
+    alwaysOnTop: true,
+    webPreferences: {
+      preload: path.join(__dirname, './frontEnd/preload.js')
+    }
   })
 
   // 嵌入网页
@@ -24,14 +32,20 @@ app.whenReady().then(() => {
   createWindow();
 
   // 当所有的窗口都被关闭时触发
+  // 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 因此, 通常
+  // 对应用程序和它们的菜单栏来说应该时刻保持激活状态, 
+  // 直到用户使用 Cmd + Q 明确退出
   app.on("window-all-closed", () => {
-    if(process.platform != "darwin") {
+    if (process.platform != "darwin") {
       app.quit();
     }
   })
 
   // macOS 激活时事件
   app.on("activate", () => {
-    createWindow();
+    // 在 macOS 系统内, 如果没有已开启的应用窗口，点击托盘图标时通常会重新创建一个新窗口
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
   })
 })
