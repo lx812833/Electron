5adf0cd42b195b85d24a233fac2c500f95782281
longfuyang <longfuyang@aosign.cn>
2023-02-23 13:55:31 +0800

    init：Electron + Vue3 + Vite

---
+++ electron-app/src/renderer/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Versions />
+  <svg class="hero-logo" viewBox="0 0 900 300">
+    <use xlink:href="./assets/icons.svg#electron" />
+  </svg>
+  <h2 class="hero-text">Electron + Vue3 + Vite</h2>
+</template>
+
+<script setup lang="ts">
+import Versions from './components/Versions.vue'
+</script>
+
+<style lang="less">
+@import './assets/css/styles.less';
+</style>
--- viteElectron/electron/main.cjs
+++ viteElectron/electron/main.cjs
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require("electron");
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
-    width: 600,
-    height: 600,
+    width: 300,
+    height: 300,
     alwaysOnTop: true,
   })
 
