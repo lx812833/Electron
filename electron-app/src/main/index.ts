51a77b1c0fe0fd9c77102f4d53834657863325a0
longfuyang <longfuyang@aosign.cn>
2023-02-24 11:15:13 +0800

    feat：菜单栏

--- electron-app/src/main/index.ts
+++ electron-app/src/main/index.ts
@@ -4,6 +4,9 @@ import { electronApp, optimizer, is } from '@electron-toolkit/utils';
 import icon from '../../resources/icon.png?asset';
 
 function createWindow(): void {
+  // 是否是苹果系统
+  const isMac = process.platform === 'darwin';
+
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     x: screen.getPrimaryDisplay().workAreaSize.width - 900,
@@ -61,11 +64,38 @@ function createWindow(): void {
   // 创建菜单
   const menu = Menu.buildFromTemplate([
     {
-      label: '菜单',
+      label: 'Honduran',
       submenu: [
         {
+          label: '打开新窗口',
+          click: () => {
+            const win = new BrowserWindow({
+              width: 300,
+              height: 300,
+            })
+            win.loadURL('https://www.bilibili.com/')
+          },
+        },
+        {
+          type: 'separator', // 分割线
+        },
+        {
+          label: '增加', // 渲染进程触发主进程通信
           click: () => mainWindow.webContents.send('increment', 1),
-          label: '增加',
+        },
+        {
+          label: '退出',
+          click: async () => app.quit(),
+          accelerator: 'CommandOrControl+q',  // 定义快捷键
+        },
+        isMac ? { label: '关闭', role: 'close' } : { role: 'quit' },
+      ],
+    },
+    {
+      label: '在线网站',
+      submenu: [
+        {
+          label: '哔哩哔哩',
         },
       ],
     },
