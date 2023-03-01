0fe6d1d379164dd6c0be16f60be662924f668c88
longfuyang <longfuyang@aosign.cn>
2023-03-01 15:43:50 +0800

    feat：托盘tray

--- electron-app/src/main/index.ts
+++ electron-app/src/main/index.ts
@@ -1,4 +1,4 @@
-import { app, shell, ipcMain, Notification, Menu, BrowserWindow, screen, dialog, nativeTheme } from 'electron';
+import { app, shell, ipcMain, Notification, Tray, nativeImage, Menu, BrowserWindow, screen, dialog, nativeTheme } from 'electron';
 import { join } from 'path';
 import { electronApp, optimizer, is } from '@electron-toolkit/utils';
 import icon from '../../resources/icon.png?asset';
@@ -65,8 +65,22 @@ const createWindow = () => {
 
   applicationMenu(mainWindow);
 
+  // 托盘
+  const trayIcon = nativeImage.createFromPath('../../resources/favicon.ico');
+  const tray = new Tray(trayIcon);
 
-  mainWindow.webContents.openDevTools();
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio' },
+    { label: 'Item2', type: 'radio' },
+    { label: 'Item3', type: 'radio', checked: true },
+    { label: 'Item4', type: 'radio' }
+  ])
+
+  tray.setContextMenu(contextMenu);
+  tray.setToolTip('This is my application');
+  tray.setTitle('This is my title');
+
+  // mainWindow.webContents.openDevTools();
   // 将窗口移动到屏幕中心
   // mainWindow.center();
 }
